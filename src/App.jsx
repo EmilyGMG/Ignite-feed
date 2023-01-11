4def57496a67eaa9979b6ae2baad2b1c2d2d4007
EmilyGMG <emily.maria2010@hotmail.com>
2023-01-11 20:46:49 -0300

    creating sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,26 @@ import { Post } from './Post'
 import './global.css'
 
 import styles from './App.module.css'
+import { Sidebar } from './components/Sidebar'
 
 function App() {
 
   return (
     <>
       <Header />
-      <div className={styles.wrapper}></div>
+      <div className={styles.wrapper}>
+         <Sidebar />
+        <main>
+          <Post 
+            author="author name"
+            content="lorem text"
+          />
+          <Post 
+            author="author name 2"
+            content="lorem text 2"
+          />
+        </main>
+      </div>
     </>
   )
 }
