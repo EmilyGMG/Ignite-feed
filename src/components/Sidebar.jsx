697061216431ca3f917d2cd0f7e31885e3ea417e
EmilyGMG <emily.maria2010@hotmail.com>
2023-01-12 17:48:10 -0300

    ending sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 
 import styles from './Sidebar.module.css'
+import { PencilLine } from 'phosphor-react'
 
 export function Sidebar() {
   return (
@@ -7,12 +8,16 @@ export function Sidebar() {
       <img className={styles.cover} src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=40" />
 
       <div className={styles.profile}>
+        <img className={styles.avatar} src='https://plus.unsplash.com/premium_photo-1669930763060-799cb8e82293?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80' alt="profile image"/>
         <strong>Name User</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
-        <a href='#'>Editar seu perfil</a>
+        <a href='#'>
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   )
